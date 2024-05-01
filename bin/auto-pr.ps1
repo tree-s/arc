c0120c7d93f00c71095bb1329413f8819b2ab5f9
Michael J. MacMullin <mike.macmullin@gmail.com>
2024-05-01 11:19:06 -0230

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "tree-s/arc:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
